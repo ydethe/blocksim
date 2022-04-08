ce57b65d4320d793ab191f5c580207a1f69a9fae
YANN BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2022-04-08 15:25:44 +0200

    up doc

--- blocksim/constants.py
+++ blocksim/constants.py
@@ -1,19 +1,44 @@
 from numpy import pi
 
 
-# Constantes
+#: Boltzmann constant
 kb = 1.380649e-23
+
+#: Speed of light in vacuum
 c = 299792458.0
+
+#: Equatorial Earth radius (WGS84)
 Req = 6378137.0
+
+#: Polar Earth radius (WGS84)
 Rpo = 6356752.3
+
+#: Inverse of flattening (WGS84)
 rf = 298.257223563
+
+#: Earth sideral revolution pulsation
 omega = 7.2921151467064e-5
+
+#: Gravitationnal parameter
 mu = 3.986004418e14
+
+#: Earth sideral day
 jour_sideral = 2 * pi / omega
+
+#: Earth stellar day
 jour_stellaire = 86400.0
+
+#: Earth J2 coefficient
 J2 = 1.08263e-3
 
+#: Astronomical Unit
 AU_M = 149597870700.0  # per IAU 2012 Resolution B2
+
+#: Astronomical Unit in km
 AU_KM = 149597870.700
+
+#: Number of arcseconds in a full circle
 ASEC360 = 1296000.0
+
+#: Stellar day
 DAY_S = 86400.0
