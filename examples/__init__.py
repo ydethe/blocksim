af4000296f9174bdad933d4c230da1181f8dc877
YANN BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2022-04-11 10:27:40 +0200

    Added example generation

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,31 +1,28 @@
-"""
-.. include:: example_filtering.md
-
-"""
-
-# https://nbconvert.readthedocs.io/en/latest/nbconvert_library.html
-
 import os
 import nbformat
 from nbconvert import MarkdownExporter
 
 
-
-def convert_nb(pth, exporter):
-    with open(os.path.join("examples",pth),'r') as f:
-        nb = nbformat.reads(f.read(), as_version=4)
-
-    (body, resources) = exporter.from_notebook_node(nb)
-
-    pth_dst=pth.replace('.ipynb','.md')
-    with open(os.path.join("examples",pth_dst),'w') as f:
-        f.write(body)
-    
-    for pth_img in resources['outputs'].keys():
-        f = open(os.path.join("htmldoc","examples",pth_img),'wb')
-        f.write(resources['outputs'][pth_img])
-        f.close()
+# https://nbconvert.readthedocs.io/en/latest/nbconvert_library.html
 
 exporter=MarkdownExporter()
-convert_nb('example_filtering.ipynb', exporter)
 
+for dirpath,dirnames,filenames in os.walk('examples'):
+    for fic in filenames:
+        if not fic.endswith('.ipynb'):
+            continue
+
+        print(os.path.join(dirpath,fic))
+        with open(os.path.join(dirpath,fic),'r') as f:
+            nb = nbformat.reads(f.read(), as_version=4)
+
+        (body, resources) = exporter.from_notebook_node(nb)
+
+        pth_dst=fic.replace('.ipynb','.md')
+        with open(os.path.join(dirpath,pth_dst),'w') as f:
+            f.write(body)
+        
+        for pth_img in resources['outputs'].keys():
+            f = open(os.path.join("htmldoc","examples",pth_img),'wb')
+            f.write(resources['outputs'][pth_img])
+            f.close()
