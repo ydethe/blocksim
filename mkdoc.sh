192fd1638ccc81d6c66b974c3382836ab6a0afa1
YANN BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2022-04-11 10:56:09 +0200

    Added example generation

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,5 +1,6 @@
 # https://pypi.org/project/eastereig/
-mkdir -p htmldoc
+mkdir -p htmldoc/examples
+python3 examples/__init__.py
 pdoc --html --force --config latex_math=True -o htmldoc blocksim
 pyreverse -s0 blocksim -k --colorized -p blocksim -m no --ignore=exceptions.py,LogFormatter.py,DatabaseModel.py,CSVLogger.py,PickleLogger.py,PsqlLogger.py,XlsLogger.py -d htmldoc # In pylint package
 dot -Tpng htmldoc/classes_blocksim.dot -o htmldoc/blocksim/classes.png
