35cc44ea2683abb636acb626525a86f750dca297
Yann de The <ydethe@gmail.com>
2022-04-19 20:43:11 +0200

    Test CI

--- mkdoc.sh
+++ mkdoc.sh
@@ -6,5 +6,4 @@ python3 examples/__init__.py
 pdoc --html --force --config latex_math=True -o htmldoc blocksim examples
 pyreverse -s0 blocksim -k --colorized -p blocksim -m no --ignore=exceptions.py,LogFormatter.py,DatabaseModel.py,CSVLogger.py,PickleLogger.py,PsqlLogger.py,XlsLogger.py -d htmldoc # In pylint package
 dot -Tpng htmldoc/classes_blocksim.dot -o htmldoc/blocksim/classes.png
-nohup python3 -m http.server 2> error.log &
-
+# nohup python3 -m http.server 2> error.log &
