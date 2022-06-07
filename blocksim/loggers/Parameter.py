281f1ce3f48876cbc629a95aee91a4511238bdd4
Yann de The <ydethe@gmail.com>
2022-06-07 15:15:43 +0200

    Added Parameter.getTypeDB

--- blocksim/loggers/Parameter.py
+++ blocksim/loggers/Parameter.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 from typing import List, Any
 
+import numpy as np
+
 
 @dataclass(init=True)
 class Parameter:
@@ -18,3 +20,20 @@ class Parameter:
         else:
             res = f"Parameter '{self.name}' ({self.unit}): {self.description}"
         return res
+
+    def getTypeDB(self) -> str:
+        val = self.dtype()
+        if isinstance(val, bytes):
+            typ = val
+        elif isinstance(val, (int, np.int8, np.int16, np.int32, np.int64)):
+            typ = "integer"
+        elif isinstance(val, (float, np.float16, np.float32, np.float64)):
+            typ = "float"
+        elif isinstance(val, (complex, np.complex64, np.complex128)):
+            typ = "complex"
+        elif isinstance(val, (bool)):
+            typ = "boolean"
+        else:
+            raise ValueError("Impossible to determine type of %s" % val)
+
+        return typ
